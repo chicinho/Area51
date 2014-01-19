73667d7b2b42400e5217849a0b7c7a782fd2e1ab
chicinho <chicinho54@gmail.com>
2014-01-19 20:28:01 +0100

    Initial draft

---
+++ SRP/EmailNotificationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRP
+{
+    public class EmailNotificationService : INotificationSerivce
+    {
+        public bool Notify()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
